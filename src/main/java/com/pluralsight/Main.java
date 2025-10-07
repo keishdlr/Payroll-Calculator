b3e50ec32107068e348b620febb08e5cf29ecff5
keisha De La Rosa <keishdlr@gmail.com>
2025-10-07 14:35:02 -0400

    constructors, getters and setters, and csv file added

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.io.*;
+public class Main {
+    String filepath = "employees.csv"; //file path
+    try(BufferedReader bufReader = new BufferedReader(new FileReader("employee.csv"));{
+
+
+    }
+
+
+
+
+
+
+
+}
