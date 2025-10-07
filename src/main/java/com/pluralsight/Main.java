f463f69674eb731e9c64741b59d35a167dafa46e
keisha De La Rosa <keishdlr@gmail.com>
2025-10-07 15:13:33 -0400

    step 2 making the program work

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,37 @@
 package com.pluralsight;
 
+import javax.sound.sampled.Line;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     String filepath = "employees.csv"; //file path
-    try(BufferedReader bufReader = new BufferedReader(new FileReader("employee.csv"));{
+    try{
+        BufferedReader bufReader = new BufferedReader(new FileReader("employee.csv")); //file reader object and buffer reader to manage input
+        String input; //Placeholder for every line of text to be read
 
+        while (input = bufReader.readLine() != null); // Read until there is no more lines
+       // Split it into individual fields using the | character as the delimiter
+        String[] attributes = line.split ("\\|");
+        // Copy the values from the tokens array into variables that match the data
+        //and then use them to create a new Employee object
+        String id = attributes[0];
+        String name = attributes[1];
+        double hoursWorked = attributes[2];
+        double payRate = [3];
 
+        //employee object
+
+        // close the stream and release the resources
+        bufReader.close();
+    }
+      catch(IOException e) {
+        // display stack trace if there was an error
+        e.printStackTrace();
     }
 
-
-
-
-
-
-
+    public Main() throws IOException {
+    }
 }
